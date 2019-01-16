832c07a62806f483a88d47d143b58f73721a36f8
kurogane-no-shiro <thrashattack1127@gmail.com>
2019-01-16 13:47:37 +0900

    Groupsのビューファイル（haml）を作成。および、groups/newへのリンク作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only:  [:edit, :update]
+  resources :groups, only: [:new, :edit, :create, :update] do
+  end
 end
