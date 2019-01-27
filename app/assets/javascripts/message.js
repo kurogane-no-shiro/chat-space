49eb04c268c402961496d832b279bc813d1bd537
kurogane-no-shiro <thrashattack1127@gmail.com>
2019-01-27 15:40:08 +0900

    Jsファイルの手直し
    スクロールの動作を確認。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,27 @@
+
 function buildHTML(message){
   var html = `
                 <div class="message">
+                <div class="upper-message">
                 <div class="upper-message__user.name">
                   ${ message.user_name }
                   </div>
                 <div class="upper-message__date">
                   ${ message.time}
+                  </div>
+                  </div>
+                <div class="lower-message">
                 <p class="lower-message__content">
-                  ${ message.content }`
+                  ${ message.content }
+                  </div>
+                  </div>`
   return html;
   }
 
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
+    e.stopPropagation();
     var url = $(this).attr('action');
     var formData = new FormData($(this).get(0));
     $.ajax({
@@ -29,7 +37,6 @@ $(function(){
       $('.messages').append(html);
       $('.form__message').val('')
       $('.form__submit').prop('disable', false);
-          e.stopPropagation();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       })
     .fail(function(message){
