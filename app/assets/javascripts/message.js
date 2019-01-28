3f96a5c0250d5975911a35f59b239827edf9988e
kurogane-no-shiro <thrashattack1127@gmail.com>
2019-01-28 11:48:53 +0900

    非同期通信_画像の投稿も出来る様になった

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,25 @@
-
 function buildHTML(message){
+  var show_image = (message.image) ? `<img src="${ message.image }" class="lower-message__image">` : `<img src="" class="">`;
   var html = `
-                <div class="message">
-                <div class="upper-message">
+            <div class="message">
+              <div class="upper-message">
                 <div class="upper-message__user.name">
-                  ${ message.user_name }
-                  </div>
+                ${ message.user_name }
+                </div>
                 <div class="upper-message__date">
-                  ${ message.time}
-                  </div>
-                  </div>
-                <div class="lower-message">
+                ${ message.time}
+                </div>
+              </div>
+              <div class="lower-message">
                 <p class="lower-message__content">
-                  ${ message.content }
-                  </div>
-                  </div>`
+                ${ message.content }
+                <div class="lowewr-message__image">
+                ${ show_image }
+                </div>
+              </div>
+            </div>`
   return html;
-  }
+}
 
 $(function(){
   $('#new_message').on('submit', function(e){
@@ -37,6 +40,7 @@ $(function(){
       $('.messages').append(html);
       $('.form__message').val('')
       $('.form__submit').prop('disable', false);
+      $('#new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       })
     .fail(function(message){
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -3,3 +3,4 @@ json.user_id @message.user.id
 json.group_id @message.group.id
 json.user_name @message.user.name
 json.time @message.created_at
+json.image @message.image.url
